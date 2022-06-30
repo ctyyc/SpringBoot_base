b7787cfdfb06c38bc113e00c80d86545dc9e427d
Minkoo Park <ctyyc@naver.com>
2022-06-30 23:47:13 +0900

    use Memory

---
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복회원 검증 (이름 기준)
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("Already exists!");
+                });
+    }
+
+    // 전체회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
---
+++ demo/src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void join() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void validateDuplicateMember() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("Already exists!");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
